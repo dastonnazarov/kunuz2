000f71b7272ec61be75339b33423d9bfa57c9b61
dastonnazarov <dastonnazarov0910@gmail.com>
2023-05-09 11:25:28 +0500

    CommentLikeController update request

--- src/main/java/com/example/kunuz2/controller/CommentLikeController.java
+++ src/main/java/com/example/kunuz2/controller/CommentLikeController.java
@@ -3,6 +3,7 @@ package com.example.kunuz2.controller;
 import com.example.kunuz2.dto.jwt.JwtDTO;
 import com.example.kunuz2.service.CommentLikeService;
 import com.example.kunuz2.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,23 +17,26 @@ public class CommentLikeController {
 
     @GetMapping("/like/{id}")
     public ResponseEntity<Boolean> like(@PathVariable("id") Integer id,
-                                        @RequestHeader("Authorization") String authorization) {
-        JwtDTO jwt = JwtUtil.getJwtDTO(authorization);
-        return ResponseEntity.ok(commentLikeService.like(id, jwt.getId()));
+                                        HttpServletRequest request) {
+       JwtUtil.checkForRequiredRole(request);
+        Integer jwtId = (Integer) request.getAttribute("id");
+        return ResponseEntity.ok(commentLikeService.like(id, jwtId));
     }
 
     @GetMapping("/dislike/{id}")
     public ResponseEntity<Boolean> dislike(@PathVariable("id") Integer id,
-                                        @RequestHeader("Authorization") String authorization) {
-        JwtDTO jwt = JwtUtil.getJwtDTO(authorization);
-        return ResponseEntity.ok(commentLikeService.disLike(id, jwt.getId()));
+                                           HttpServletRequest request) {
+        JwtUtil.checkForRequiredRole(request);
+        Integer jwtId = (Integer) request.getAttribute("id");
+        return ResponseEntity.ok(commentLikeService.disLike(id, jwtId));
     }
 
 
     @DeleteMapping("/deletelike/{id}")
     public ResponseEntity<Boolean> delete(@PathVariable("id") Integer id,
-                                        @RequestHeader("Authorization") String authorization) {
-        JwtDTO jwt = JwtUtil.getJwtDTO(authorization);
-        return ResponseEntity.ok(commentLikeService.delete(id, jwt.getId()));
+                                        HttpServletRequest request) {
+        JwtUtil.checkForRequiredRole(request);
+        Integer jwtId = (Integer) request.getAttribute("id");
+        return ResponseEntity.ok(commentLikeService.delete(id, jwtId));
     }
 }
