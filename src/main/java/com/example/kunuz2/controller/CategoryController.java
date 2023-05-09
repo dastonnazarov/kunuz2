093ba4ba1268e9fed0d91c2f51dfff2b43b9a46d
dastonnazarov <dastonnazarov0910@gmail.com>
2023-05-09 10:59:09 +0500

    CategoryController update request

--- src/main/java/com/example/kunuz2/controller/CategoryController.java
+++ src/main/java/com/example/kunuz2/controller/CategoryController.java
@@ -5,10 +5,13 @@ import com.example.kunuz2.dto.jwt.JwtDTO;
 import com.example.kunuz2.enums.ProfileRole;
 import com.example.kunuz2.service.CategoryService;
 import com.example.kunuz2.util.JwtUtil;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.HandlerTypePredicate;
 
 @RestController
 @RequestMapping("/api/v1/category")
@@ -18,31 +21,35 @@ public class CategoryController {
 
     @PostMapping({"", "/"})
     public ResponseEntity<Integer> create(@RequestBody CategoryDTO dto,
-                                          @RequestHeader("Authorization") String authorization) {
-        JwtDTO jwtDTO = JwtUtil.getJwtDTO(authorization, ProfileRole.ADMIN);
-        return ResponseEntity.ok(categoryService.create(dto, jwtDTO.getId()));
+                                          HttpServletRequest request) {
+        JwtUtil.checkForRequiredRole(request, ProfileRole.ADMIN);
+        Integer jwtId = (Integer) request.getAttribute("id");
+        return ResponseEntity.ok(categoryService.create(dto, jwtId));
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<Boolean> update(@PathVariable("id") Integer id,
                                           @RequestBody CategoryDTO categoryDto,
-                                          @RequestHeader("Authorization") String authorization) {
-        JwtDTO jwtDTO = JwtUtil.getJwtDTO(authorization, ProfileRole.ADMIN);
+                                          HttpServletRequest request) {
+        JwtUtil.checkForRequiredRole(request, ProfileRole.ADMIN);
+        Integer jwtId = (Integer) request.getAttribute("id");
         return ResponseEntity.ok(categoryService.update(id, categoryDto));
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Boolean> deleteById(@PathVariable("id") Integer id,
-                                              @RequestHeader("Authorization") String authorization) {
-        JwtDTO jwtDTO = JwtUtil.getJwtDTO(authorization, ProfileRole.ADMIN);
-        return ResponseEntity.ok(categoryService.deleteById(id, jwtDTO.getId()));
+                                              HttpServletRequest request) {
+        JwtUtil.checkForRequiredRole(request, ProfileRole.ADMIN);
+        Integer jwtId = (Integer) request.getAttribute("id");
+        return ResponseEntity.ok(categoryService.deleteById(id, jwtId));
     }
 
     @GetMapping("/list-paging")
     public ResponseEntity<Page<CategoryDTO>> getAll(@RequestParam(value = "page", defaultValue = "1") int page,
                                                     @RequestParam(value = "size", defaultValue = "2") int size,
-                                                    @RequestHeader("Authorization") String authorization) {
-        JwtDTO jwtDTO = JwtUtil.getJwtDTO(authorization, ProfileRole.ADMIN);
+                                                    HttpServletRequest request) {
+        JwtUtil.checkForRequiredRole(request, ProfileRole.ADMIN);
+        Integer jwtId = (Integer) request.getAttribute("id");
         return ResponseEntity.ok(categoryService.getAll(page, size));
     }
 }
